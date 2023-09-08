2d4c275d769ac799e866120115714542911c6e08
LilianeAssouma <lilyanassoum@gmail.com>
2023-09-08 09:00:32 +0200

    update  commit

--- script.js
+++ script.js
@@ -38,22 +38,55 @@ document.addEventListener('DOMContentLoaded', function() {
     URL.revokeObjectURL(url);
 });
 
-shareBtn.addEventListener('click',function(){
-    const svgContent=new XMLSerializer().serializeToString(barcodeSvg);
-    const blob=new Blob([svgContent],{type:'image/svg+xml'});
-    const url=URL.createObjectURL(blob);
-//web share API
-    if (navigator.share) {
-        navigator.share({
-            title: 'Barcode',
-            text: 'Check out this barcode!',
-            url: url,
-        })
-        .then(() => console.log('Shared successfully'))
-        .catch(error => console.error('Error sharing:', error));
-    }
-    URL.revokeObjectURL(url);
+shareBtn.addEventListener('click', function () {
+  const svgContent = new XMLSerializer().serializeToString(barcodeSvg);
+  const blob = new Blob([svgContent], { type: 'image/svg+xml' });
+  const url = URL.createObjectURL(blob);
+
+  // Create a share menu
+  const shareMenu = document.createElement('div');
+  shareMenu.className = 'share-menu';
+
+  // Create share via email option
+  const emailOption = document.createElement('button');
+  emailOption.textContent = 'Share via Email';
+  emailOption.addEventListener('click', function () {
+      const subject = 'Check out this barcode!';
+      const body = `Here's the barcode: ${url}`;
+      const mailtoLink = `mailto:?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+      window.location.href = mailtoLink;
+  });
+
+  // Create print option
+  const printOption = document.createElement('button');
+  printOption.textContent = 'Print';
+  printOption.addEventListener('click', function () {
+      const printWindow = window.open('', '', 'width=600,height=600');
+      printWindow.document.open();
+      printWindow.document.write(`<img src="${url}" />`);
+      printWindow.document.close();
+      printWindow.print();
+      printWindow.close();
+  });
+
+  // Append the options to the share menu
+  shareMenu.appendChild(emailOption);
+  shareMenu.appendChild(printOption);
+
+  // Append the share menu to the body
+  document.body.appendChild(shareMenu);
+
+  // Remove the share menu after clicking an option
+  const removeShareMenu = function () {
+      document.body.removeChild(shareMenu);
+  };
+
+  emailOption.addEventListener('click', removeShareMenu);
+  printOption.addEventListener('click', removeShareMenu);
 });
+
+
+
   });
 
 
